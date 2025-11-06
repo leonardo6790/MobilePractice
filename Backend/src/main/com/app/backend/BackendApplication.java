4f7cfbf04440284f38b08924b453925dd99a561a
leoanrdo <leonardobautista67@gmail.com>
2025-11-06 17:27:03 -0500

    first commit

---
+++ Backend/src/main/com/app/backend/BackendApplication.java
@@ -0,0 +1,12 @@
+package com.app.backend;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class BackendApplication {
+    Run | Debug
+    public static void main(Spring[] args){
+        SpringApplication.run(BackendApplication.class, args);
+    }
+}
